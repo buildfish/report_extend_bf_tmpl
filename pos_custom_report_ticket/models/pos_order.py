706c870da4276caea6d9289315eace8876c3cbb8
dperalta <david.2964.erls@gmail.com>
2018-12-19 14:58:10 -0500

    Update report pos ticket

--- pos_custom_report_ticket/models/pos_order.py
+++ pos_custom_report_ticket/models/pos_order.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models
-from datetime import datetime
+import pytz
+
+from odoo import models, fields
 
 
 class pos_order(models.Model):
     _inherit = "pos.order"
 
     def custom_report(self, **kw):
-        date = datetime.strptime(self.date_order, '%Y-%m-%d %H:%M:%S')
+        user_tz = pytz.timezone(self.env.context.get('tz') or self.env.user.tz or 'UTC')
+        date = fields.Datetime.from_string(self.date_order).replace(tzinfo=pytz.utc).astimezone(user_tz)
+
         values = {
             "day": date.day,
             "month": date.month,
